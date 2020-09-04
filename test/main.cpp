c607d69f9f8b9a9b4fc3a601e88f85cb939c0379
YanB25 <yanb20@mails.tsinghua.edu.cn>
2020-09-04 15:29:53 +0800

    refactor(build): better cmake

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,23 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-04 15:09:54
+ * @LastEditTime: 2020-09-04 15:18:35
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /Sherman/test/main.cpp
+ */
 #include <iostream>
+#include <boost/version.hpp>
 
-int main() {
+int main()
+{
     std::cout << "Hello World!\n";
-
+    std::cout << "Boost version: "
+              << BOOST_VERSION / 100000
+              << "."
+              << BOOST_VERSION / 100 % 1000
+              << "."
+              << BOOST_VERSION % 100
+              << std::endl;
     return 0;
 }
