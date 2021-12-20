d11e55d47f259cc187a2976bbd94bafab2c01317
yanbin <yanb20@mails.tsinghua.edu.cn>
2021-12-20 20:52:53 +0800

    feat(script-py): script for multi-process ib_*_* running and result parsing
    Debug for a long time. ssh has a rule: it limits the unauthorized connection by (by default) 10.
    So, the ssh connection keeps disconnecting. It, in turn, causes the server to hang forever.
    I though it was the bug from subprocess, but it turns out to be the problem from ssh connection.
    It really takes me a lot of time handling them.
    
    BTW, the expr results:
    9-12 threads reach the maximum send/recv ops.
    size -> larger, ops -> slower.
    turn on the "inline" helps A LOT.

--- script-py/exec.py
+++ script-py/exec.py
@@ -1,56 +1,112 @@
-class Executor:
-    def __init__(self, machines):
-        '''
-        machines is a dict of (name, ip)
-        '''
-        self.machines = machines
-        self.out = {}
-        self.err = {}
+import subprocess
+import pprint
+import os
+import time
 
-    def run_cmd(self, cmd):
-        for (name, machine) in self.machines.items():
-            self.processes[name] = subprocess.Popen(
+class Config:
+    def __init__(self):
+        # @id to @ip that can use for ssh connection
+        self.__machines = {}
+        # @id to @cmd that is run
+        self.__cmds = {}
+        # @id to @private_data that user can attach. Config ignores this field
+        self.__priv = {}
+    def reg_machine(self, id, machine):
+        self.__machines[id] = machine
+    def reg_machines(self, ids, machines):
+        for (id, machine) in zip(ids, machines):
+            self.reg_machine(id, machine)
+    def reg_cmd(self, id, cmd):
+        self.__cmds[id] = cmd
+    def reg_cmds(self, ids, cmds):
+        for (id, cmd) in zip(ids, cmds):
+            self.reg_cmd(id, cmd)
+    def reg_machine_cmd(self, id, machine, cmd):
+        self.reg_machine(id, machine)
+        self.reg_cmd(id, cmd)
+    def show(self):
+        pprint.pprint(self.__machines)
+        pprint.pprint(self.__cmds)
+    def reg_priv(self, id, data):
+        self.__priv[id] = data
+    
+    def cmd(self, id):
+        return self.__cmds[id]
+    def ids(self):
+        return self.__machines.keys()
+    def machine(self, id):
+        return self.__machines[id]
+    def priv(self, id):
+        return self.__priv[id]
+
+
+class Executor:
+    def __init__(self, conf):
+        '''
+        machines is a dict of (@name, @ip)
+        @name is the unique string to identify a machine.
+        @ip is the address used for ssh connection.
+        '''
+        self.__conf = conf
+        self.__out = {}
+        self.__err = {}
+        self.__processes = {}
+
+    def run(self):
+        for id in self.__conf.ids():
+            machine = self.__conf.machine(id)
+            cmd = self.__conf.cmd(id)
+            pprint.pprint(cmd)
+            self.__processes[id] = subprocess.Popen(
                 [
                     'ssh',
                     machine,
-                    *cmd
-                ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    *cmd,
+                ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+            
 
-    def wait(self, time=1):
-        while True:
-            all_finish = True
-            for (name, p) in self.processes.items():
-                if p.poll() is None:
-                    try:
-                        p.wait(time)
-                    except subprocess.TimeoutExpired:
-                        all_finish = False
-                        self.collect_output(name, p)
-                else:
-                    print("ERROR: handle p.poll() is not None")
-                    exit(-1)
-            if all_finish:
-                break
+    def wait(self, timeout=30):
+        '''
+        call me to wait until all the process finishes
+        '''
+        fast_failure_rollback = False
+        for (name, p) in self.__processes.items():
+            try:
+                stdout, stderr = p.communicate(input=None, timeout=0 if fast_failure_rollback else timeout)
+                self.collect_output(name, stdout, stderr)
+            except subprocess.TimeoutExpired:
+                fast_failure_rollback = True
+                p.kill()
+                stdout, stderr = p.communicate(input=None, timeout=None)
+                self.collect_output(name, stdout, stderr)
 
-    def collect_output(self, name, p):
-        stdout, stderr = p.communicate()
-        if name not in self.out:
-            self.out[name] = ''
-        if name not in self.err:
-            self.err[name] = ''
-        self.out[name] += stdout.read().decode('ascii')
-        self.err[name] += stdout.read().decode('ascii')
+    def collect_output(self, name, stdout, stderr):
+        if name not in self.__out:
+            self.__out[name] = ''
+        if name not in self.__err:
+            self.__err[name] = ''
+        self.__out[name] += stdout.decode('ascii')
+        self.__err[name] += stderr.decode('ascii')
+        if (len(self.__err[name]) > 0):
+            print("{} ERR: {}".format(name, self.__err[name]))
 
     def to_file(self, dir):
-        for name, out in self.out:
-            with open(os.path.join(dir, name + ".stdout.log"), w) as f:
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+        for name, out in self.__out.items():
+            with open(os.path.join(dir, name + ".stdout.log"), 'w') as f:
                 f.write(out)
-        for name, err in self.err:
-            with open(os.path.join(dir, name + ".stdout.log"), w) as f:
+        for name, err in self.__err.items():
+            with open(os.path.join(dir, name + ".stderr.log"), 'w') as f:
                 f.write(err)
 
     def out(self, name):
-        return self.out[name]
+        return self.__out[name]
 
     def err(self, name):
-        return self.err[name]
+        return self.__err[name]
+    
+    def print_err_if_exists(self):
+        for id, err in self.__err.items():
+            if len(err) != 0:
+                print("{} ERR: {}".format(id, err))
---
+++ script-py/run-send.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+
+import exec
+import itertools
+
+inets = []
+with open('inets.conf') as f:
+    inets = f.read().strip().split('\n')
+    inets = [item.strip() for item in inets]
+
+def to_name(inet, pid):
+    return '{}.pid={}'.format(inet, pid)
+def init_conf(conf, inets, process_nr):
+    for inet in inets:
+        for pid in range(process_nr):
+            name = to_name(inet, pid)
+            conf.reg_machine(name, inet)
+
+
+def gen_ib_send_bw_cmd(duration, qpn, size, post_list_size, cq_mod, port=18515, tx_depth=128, mr_per_qp=False, odp=False, use_hugepages=False, is_client=False, inline_size=0, inline_recv=0):
+    cmd = [
+        'unbuffer',
+        'ib_send_bw',
+        '--connection=RC',
+        '--duration {}'.format(duration),
+        '--qp {}'.format(qpn),
+        '--size {}'.format(size),
+        '--tx-depth {}'.format(tx_depth),
+        '--post_list {}'.format(post_list_size),
+        '--cq-mod {}'.format(cq_mod),
+        '--port {}'.format(port),
+        # '--inline_size 8', # soo slow after setting this
+        # '--inline_recv 8', # sooo slow after setting this
+        '--CPU-freq',
+    ]
+    if mr_per_qp:
+        cmd.append("--mr_per_qp")
+    if odp:
+        cmd.append('--odp')
+    if use_hugepages:
+        cmd.append('--use_hugepages')
+    if is_client:
+        cmd = ['sleep 2;'] + cmd[:]
+    if inline_size != 0:
+        cmd.append('--inline_size {}'.format(inline_size))
+    if inline_recv != 0:
+        cmd.append('--inline_recv {}'.format(inline_recv))
+    return cmd
+
+def collect(map, name, p):
+    if name not in map:
+        map[name] = ''
+    map[name] += p.stdout.read().decode("ascii")
+    print("! collect {}. get {}".format(name, map[name]))
+
+CSV_FILE = "fetched/ib_send_bw.csv"
+
+if __name__ == '__main__':
+    with open(CSV_FILE, "w") as f:
+        f.write("tx-length,process_nr,qpn,size,inline,BW-average(MB/s),MsgRate(Mpps)\n")
+
+
+    DURATION = 10
+    DEFAULT_PORT = 18515
+    # thread_nrs = [1, 2, 4, 6, 8, 10, 12, 14, 16, 32] # max at 10
+    thread_nrs = [8, 10, 12]
+    tx_lengths = [128]
+    qpns = [1]
+    inlines = [True, False]
+    # sizes = [1]
+    sizes = [1, 8, 28, 32, 36, 40, 48, 64]
+    for (thread_nr, tx_length, qpn, size, inline) in itertools.product(thread_nrs, tx_lengths, qpns, sizes, inlines):
+        conf = exec.Config()
+        init_conf(conf, inets, thread_nr)
+
+        for inet in inets:
+            is_client = inet != inets[0]
+            for pid in range(thread_nr):
+                if inline:
+                    inline_size = size
+                    inline_recv = size
+                else:
+                    inline_size = 0
+                    inline_recv = 0
+                cmd = gen_ib_send_bw_cmd(DURATION, qpn, size, 64, 64, port=DEFAULT_PORT + pid, mr_per_qp=True, use_hugepages=True, is_client=is_client, inline_size=inline_size, inline_recv=inline_recv)
+                if is_client:
+                    cmd.append(' {}'.format(inets[0]))
+                cmd.append(" |& tee {}.log".format(to_name(inet, pid)))
+                conf.reg_cmd(to_name(inet, pid), cmd)
+
+
+        executor = exec.Executor(conf)
+        executor.run()
+        executor.wait()
+        sum_bw = 0
+        sum_msg_rate = 0
+        for id in conf.ids():
+            out = executor.out(id)
+            err = executor.err(id)
+
+            if conf.machine(id) == inets[0]:
+                continue
+            try:
+                ls = out.split('\n')
+                byte_nr, iterations, bw_peak, bw_avg, msg_rate = ls[-3].split()
+                sum_bw += float(bw_avg)
+                sum_msg_rate += float(msg_rate)
+            except:
+                print("{}: Err when parsing output. stderr is \n{}".format(id, err))
+        with open("fetched/ib_send_bw.csv", "a") as f:
+            f.write('{}, {}, {},{},{}, {},{}\n'.format(
+                tx_length, thread_nr, qpn, size, inline, sum_bw, sum_msg_rate))
+        executor.to_file("fetched/{}".format(thread_nr))
+        executor.print_err_if_exists()
