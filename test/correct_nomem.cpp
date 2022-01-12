3afb38bfd783018026402c87a62adb639b8a0341
yanbin <yanb20@mails.tsinghua.edu.cn>
2022-01-12 15:14:57 +0800

    done: test_all.sh script

--- test/correct_nomem.cpp
+++ test/correct_nomem.cpp
@@ -41,24 +41,24 @@ void expect(const char *lhs_buf, const char *rhs_buf, size_t size)
 
 void server(std::shared_ptr<DSM> dsm)
 {
-    LOG(INFO) << "Because dsm->send/recv may lose packet. This does not work.";
-    // size_t remain_sync = kRpcNr;
-    // size_t index = 0;
-    // while (remain_sync > 0)
-    // {
-    //     size_t should_sync = std::min(remain_sync, kSyncBatch);
-    //     for (size_t i = 0; i < should_sync; ++i)
-    //     {
-    //         uint32_t rkey = 0xabcdef;
-    //         dsm->send((char *) &rkey, sizeof(uint32_t), kClientNodeId);
-    //         index++;
-    //     }
-    //     // wait and sync
-    //     dsm->recv();
-    //     remain_sync -= should_sync;
-    // }
+    size_t remain_sync = kRpcNr;
+    size_t index = 0;
+    while (remain_sync > 0)
+    {
+        size_t should_sync = std::min(remain_sync, kSyncBatch);
+        for (size_t i = 0; i < should_sync; ++i)
+        {
+            uint32_t rkey = 0xabcdef;
+            dsm->reliable_send(
+                (char *) &rkey, sizeof(uint32_t), kClientNodeId, 0);
+            index++;
+        }
+        // wait and sync
+        dsm->reliable_recv(0, nullptr, 1);
+        remain_sync -= should_sync;
+    }
 
-    dsm->recv();
+    dsm->reliable_recv(0, nullptr, 1);
 }
 // Notice: TLS object is created only once for each combination of type and
 // thread. Only use this when you prefer multiple callers share the same
@@ -83,26 +83,28 @@ uint64_t rand_int(uint64_t min, uint64_t max)
 
 void client(std::shared_ptr<DSM> dsm)
 {
-    // size_t remain_mw = kRpcNr;
-    // size_t done_work = 0;
-    // while (remain_mw > 0)
-    // {
-    //     size_t should_recv = std::min(remain_mw, kSyncBatch);
-    //     for (size_t i = 0; i < should_recv; ++i)
-    //     {
-    //         [[maybe_unused]] uint32_t rkey = *(uint32_t *) dsm->recv();
-    //     }
-    //     dsm->send(nullptr, 0, kServerNodeId);
-    //     remain_mw -= should_recv;
+    size_t remain_mw = kRpcNr;
+    size_t done_work = 0;
+    char recv_buffer[1024];
+    while (remain_mw > 0)
+    {
+        size_t should_recv = std::min(remain_mw, kSyncBatch);
+        for (size_t i = 0; i < should_recv; ++i)
+        {
+            dsm->reliable_recv(0, recv_buffer, 1);
+            [[maybe_unused]] uint32_t rkey = *(uint32_t *) recv_buffer;
+        }
+        dsm->reliable_send(nullptr, 0, kServerNodeId, 0);
+        remain_mw -= should_recv;
 
-    //     done_work += should_recv;
-    //     if (done_work > 10 * define::M)
-    //     {
-    //         info("finish 10M. Remain %zu", remain_mw);
-    //         done_work = 0;
-    //     }
-    // }
-    // info("OK. RPC does not leak memory. perfect.");
+        done_work += should_recv;
+        if (done_work > 10 * define::M)
+        {
+            LOG(INFO) << "finish 10M. remain " << remain_mw;
+            done_work = 0;
+        }
+    }
+    LOG(INFO) << "OK. RPC does not leak memory. perfect.";
 
     const auto &buf_conf = dsm->get_server_internal_buffer();
     char *buffer = buf_conf.buffer;
@@ -130,23 +132,12 @@ void client(std::shared_ptr<DSM> dsm)
     }
 
     LOG(INFO) << "finished bind_mw total " << cnt << ", dir " << NR_DIRECTORY;
-    dsm->send(nullptr, 0, kServerNodeId);
+    dsm->reliable_send(nullptr, 0, kServerNodeId, 0);
 }
 int main(int argc, char *argv[])
 {
     google::InitGoogleLogging(argv[0]);
     gflags::ParseCommandLineFlags(&argc, &argv, true);
-    // if (argc < 3)
-    // {
-    //     fprintf(stderr, "%s [window_nr] [window_size]\n", argv[0]);
-    //     return -1;
-    // }
-    // size_t window_nr = 0;
-    // size_t window_size = 0;
-    // sscanf(argv[1], "%lu", &window_nr);
-    // sscanf(argv[2], "%lu", &window_size);
-
-    // printf("window_nr: %lu, window_size: %lu\n", window_nr, window_size);
 
     bindCore(0);
 
