302e486ac0a7cf55c8bede5074649bb9570c72e8
yanbin <yanb20@mails.tsinghua.edu.cn>
2022-03-26 22:37:44 +0800

    temp: compilable
    at least it is compilable. So work for runnable

--- test/test_race_rdma_op.cpp
+++ test/test_race_rdma_op.cpp
@@ -24,105 +24,90 @@ DEFINE_string(exec_meta, "", "The meta data of this execution");
 [[maybe_unused]] constexpr uint16_t kServerNodeId = 1;
 constexpr uint32_t kMachineNr = 2;
 
-void client_test_capacity(Patronus::pointer patronus)
+using RaceHashingT = RaceHashing<1, 2, 2>;
+using RaceHandleT = typename RaceHashingT::Handle;
+
+void client_test_capacity(Patronus::pointer p)
 {
-    std::ignore = patronus;
-    // auto allocator = std::make_shared<patronus::mem::RawAllocator>();
-    // RaceHashingConfig conf;
-    // conf.initial_subtable = initial_subtable;
-    // conf.g_kvblock_pool_size = 512_MB;
-    // conf.g_kvblock_pool_addr = malloc(conf.g_kvblock_pool_size);
+    auto meta_gaddr = p->get_object<GlobalAddress>("race:meta_gaddr", 1ms);
 
-    // using RaceHashingT = RaceHashing<1, 2, 2>;
-    // auto server_rdma_ctx = patronus::RdmaAdaptor::new_instance(0, patronus);
+    LOG(INFO) << "[bench] got meta of hashtable: " << meta_gaddr;
 
-    // CHECK(false) << "deleted reg_allocator";
+    RaceHashingHandleConfig handle_conf;
 
-    // RaceHashingT rh(server_rdma_ctx, allocator, conf);
+    auto handle_rdma_ctx =
+        patronus::RdmaAdaptor::new_instance(kServerNodeId, p);
+    RaceHandleT rhh(kServerNodeId,
+                    meta_gaddr,
+                    handle_conf,
+                    handle_rdma_ctx,
+                    nullptr /* coro */);
+    rhh.init();
 
-    // RaceHashingHandleConfig handle_conf;
+    std::string key;
+    std::string value;
+    std::map<std::string, std::string> inserted;
+    size_t succ_nr = 0;
+    size_t fail_nr = 0;
+    char key_buf[128];
+    char value_buf[128];
 
-    // auto handle_rdma_ctx =
-    //     patronus::RdmaAdaptor::new_instance(kServerNodeId, patronus);
-    // RaceHashingT::Handle rhh(kServerNodeId,
-    //                          rh.meta_gaddr(),
-    //                          handle_conf,
-    //                          handle_rdma_ctx,
-    //                          nullptr /* coro */);
-    // rhh.init();
-    CHECK(false) << "not ready";
+    HashContext ctx(0);
 
-    // std::string key;
-    // std::string value;
-    // std::map<std::string, std::string> inserted;
-    // size_t succ_nr = 0;
-    // size_t fail_nr = 0;
-    // char key_buf[128];
-    // char value_buf[128];
+    for (size_t i = 0; i < 16; ++i)
+    {
+        fast_pseudo_fill_buf(key_buf, 8);
+        fast_pseudo_fill_buf(value_buf, 8);
+        key = std::string(key_buf, 8);
+        value = std::string(value_buf, 8);
+        LOG(INFO) << "Trying to push " << key << ", " << value;
 
-    // HashContext ctx(0);
+        ctx.key = key;
+        ctx.value = value;
+        ctx.op = "put";
+        auto rc = rhh.put(key, value, &ctx);
+        if (rc == kOk)
+        {
+            inserted.emplace(key, value);
+            succ_nr++;
+            LOG(WARNING) << "[bench] pushed " << i
+                         << "-th. succ_nr: " << succ_nr
+                         << ", failed_nr: " << fail_nr;
+        }
+        else if (rc == kNoMem)
+        {
+            fail_nr++;
+        }
+        else
+        {
+            CHECK(false) << "Unknow return code: " << rc;
+        }
+    }
+    handle_rdma_ctx->put_all_rdma_buffer();
+    LOG(INFO) << "Inserted " << succ_nr << ", failed: " << fail_nr
+              << ", success rate: " << 1.0 * succ_nr / (succ_nr + fail_nr);
 
-    // LOG(INFO) << "Meta of hashtable: "
-    //           << *(RaceHashingT::MetaT *) rh.meta_addr();
+    LOG(INFO) << "Checking integrity";
 
-    // for (size_t i = 0; i < 16; ++i)
-    // {
-    //     fast_pseudo_fill_buf(key_buf, 8);
-    //     fast_pseudo_fill_buf(value_buf, 8);
-    //     key = std::string(key_buf, 8);
-    //     value = std::string(value_buf, 8);
-    //     LOG(INFO) << "Trying to push " << key << ", " << value;
+    for (const auto &[key, expect_value] : inserted)
+    {
+        ctx.key = key;
+        ctx.value = expect_value;
+        ctx.op = "get";
+        std::string get_val;
+        CHECK_EQ(rhh.get(key, get_val, &ctx), kOk);
+        CHECK_EQ(get_val, expect_value);
+    }
 
-    //     ctx.key = key;
-    //     ctx.value = value;
-    //     ctx.op = "put";
-    //     auto rc = rhh.put(key, value, &ctx);
-    //     if (rc == kOk)
-    //     {
-    //         inserted.emplace(key, value);
-    //         succ_nr++;
-    //         LOG(WARNING) << "[bench] pushed " << i
-    //                      << "-th. succ_nr: " << succ_nr
-    //                      << ", failed_nr: " << fail_nr << ", Table: " << rh;
-    //     }
-    //     else if (rc == kNoMem)
-    //     {
-    //         fail_nr++;
-    //     }
-    //     else
-    //     {
-    //         CHECK(false) << "Unknow return code: " << rc;
-    //     }
-    // }
-    // handle_rdma_ctx->put_all_rdma_buffer();
-    // LOG(INFO) << "Inserted " << succ_nr << ", failed: " << fail_nr
-    //           << ", ultilization: " << 1.0 * succ_nr / rh.max_capacity()
-    //           << ", success rate: " << 1.0 * succ_nr / (succ_nr + fail_nr);
+    LOG(INFO) << "[bench] Integrity Passed. Delete all the kvs";
 
-    // LOG(INFO) << "Checking integrity";
-
-    // for (const auto &[key, expect_value] : inserted)
-    // {
-    //     ctx.key = key;
-    //     ctx.value = expect_value;
-    //     ctx.op = "get";
-    //     std::string get_val;
-    //     CHECK_EQ(rhh.get(key, get_val, &ctx), kOk);
-    //     CHECK_EQ(get_val, expect_value);
-    // }
-    // LOG(INFO) << rh;
-
-    // LOG(INFO) << "[bench] Integrity Passed. Delete all the kvs";
-
-    // for (const auto &[key, expect_value] : inserted)
-    // {
-    //     ctx.key = key;
-    //     ctx.value = expect_value;
-    //     ctx.op = "del";
-    //     CHECK_EQ(rhh.del(key, &ctx), kOk);
-    // }
-
-    // free(conf.g_kvblock_pool_addr);
+    for (const auto &[key, expect_value] : inserted)
+    {
+        ctx.key = key;
+        ctx.value = expect_value;
+        ctx.op = "del";
+        CHECK_EQ(rhh.del(key, &ctx), kOk);
+    }
 }
 
 void server(Patronus::pointer p, size_t initial_subtable)
@@ -136,10 +121,14 @@ void server(Patronus::pointer p, size_t initial_subtable)
     conf.g_kvblock_pool_size = kKVBlockReserveSize;
     conf.g_kvblock_pool_addr = malloc(conf.g_kvblock_pool_size);
 
-    using RaceHashingT = RaceHashing<1, 2, 2>;
     auto server_rdma_ctx = patronus::RdmaAdaptor::new_instance(0, p);
 
     RaceHashingT rh(server_rdma_ctx, allocator, conf);
+
+    auto meta_gaddr = rh.meta_gaddr();
+    p->put("race:meta_gaddr", meta_gaddr, 0ns);
+
+    LOG(INFO) << "[debug] meta gaddr is " << meta_gaddr;
 
     p->server_serve(tid);
 
