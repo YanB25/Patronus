5f6af495c39120cc750d260d5572e4666c87296b
yanbin <yanb20@mails.tsinghua.edu.cn>
2022-07-06 20:41:19 +0800

    fix(script): passing parameter
    add indirection. i.e. param=$@
    So that multiple flag, i.e. --v=3 --n=2 will not break into multiple
    strings

--- script/run.sh
+++ script/run.sh
@@ -17,6 +17,7 @@ done
 # enable color and only log to terminal
 # NOTE: glog needs TERM env to enable color.
 GLOG_FLAGS="GLOG_logtostderr=1 GLOG_colorlogtostderr=1 "
+ASAN_FLAGS="ASAN_OPTIONS=halt_on_error=0"
 
 # hint to `date` and current git commit hash
 # so that the binary knows who he is.
@@ -26,14 +27,18 @@ date=$(date +'%Y-%m-%d.%H:%M:%S')
 exec_meta="${date}.${commit_hash}"
 EXEC_META_FLAGS="--exec_meta=${exec_meta}"
 
+# NOTE: this is necessary
+# the indirection will avoid breaking "--v=3 --n=1" into "--v=3" "--v=n1"
+param=$@
+
 for (( i=1; i<${#vms[@]}; i++ )); do
     echo [start service on ${vms[$i]} \(inet ${inets[$i]}\) ...]
     # ./ssh.sh ${inets[$i]} "cd ${BIN_DIR}; nohup ${BIN_DIR}/$1 1>${WORK_DIR}/LOG 2>&1 &"
-    nohup ssh root@${inets[$i]} "cd ${BIN_DIR}; source /etc/profile; export TERM='linux'; ${GLOG_FLAGS} unbuffer ${BIN_DIR}/$cmd $@ ${EXEC_META_FLAGS} 1>${WORK_DIR}/LOG 2>&1" &
+    nohup ssh root@${inets[$i]} "cd ${BIN_DIR}; source /etc/profile; export TERM='linux'; ${GLOG_FLAGS} ${ASAN_FLAGS} unbuffer ${BIN_DIR}/$cmd $param ${EXEC_META_FLAGS} 1>${WORK_DIR}/LOG 2>&1" &
     # nohup ssh root@${inets[$i]} "cd ${BIN_DIR}; source /etc/profile; export TERM='linux'; ldd ${BIN_DIR}/$cmd $@ ${EXEC_META_FLAGS} 1>${WORK_DIR}/LOG 2>&1" &
 done
 
-./ssh.sh ${inets[0]} "cd ${BIN_DIR}; source /etc/profile; export TERM='linux'; ${GLOG_FLAGS} unbuffer ${BIN_DIR}/$cmd ${EXEC_META_FLAGS} $@ 2>&1 | tee ../LOG"
+./ssh.sh ${inets[0]} "cd ${BIN_DIR}; source /etc/profile; export TERM='linux'; ${GLOG_FLAGS} ${ASAN_FLAGS} unbuffer ${BIN_DIR}/$cmd ${EXEC_META_FLAGS} $param 2>&1 | tee ../LOG"
 # ./ssh.sh ${inets[0]} "cd ${BIN_DIR}; source /etc/profile; export TERM='linux'; ldd ${BIN_DIR}/$cmd ${EXEC_META_FLAGS} $@ 2>&1 | tee ../LOG"
 # echo [Waiting peers to finish]
 # wait
