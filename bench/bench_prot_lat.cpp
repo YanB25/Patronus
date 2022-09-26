afaae9ba86eb746bf1961cb1009b6bf42cb22a65
yanbin <yanb20@mails.tsinghua.edu.cn>
2022-09-26 21:13:32 +0800

    perfect MW bench for bench_prot_lat
    bench_prot_lat.cpp:
    Now the bench for MW is perfect. It is multi-threading, and the
    situation should be very real.
    
    We find that the use of MW have locality issue:
    If we reuse MW among a small number of pool, it has better performance.
    On the other hand, round-robin within a large pool is poor.

--- bench/bench_prot_lat.cpp
+++ bench/bench_prot_lat.cpp
@@ -3,6 +3,7 @@
 
 #include "DSM.h"
 #include "Timer.h"
+#include "boost/thread/barrier.hpp"
 #include "gflags/gflags.h"
 #include "patronus/Patronus.h"
 #include "util/DataFrameF.h"
@@ -431,118 +432,164 @@ void bench_mw_do(DSM::pointer dsm,
     }
 }
 
-void bench_mw_launcher(DSM::pointer dsm,
-                       size_t bind_size,
-                       size_t test_times,
-                       size_t thread_nr)
+void do_bench_mw(DSM::pointer dsm,
+                 size_t test_times,
+                 size_t bind_size,
+                 size_t batch_size,
+                 std::vector<ibv_mw *> &constant_active_pool,
+                 std::vector<ibv_mw *> &mw_pool)
 {
-    constexpr static ssize_t kBatchSize = 4;
+    auto tid = dsm->get_thread_id();
+    auto dir_id = tid;
 
-    std::vector<std::thread> threads;
-    std::vector<std::vector<ibv_mw *>> mw_pool_threads;
-    mw_pool_threads.resize(thread_nr);
-    for (size_t i = 0; i < thread_nr; ++i)
+    ssize_t remain_task = test_times;
+    // ssize_t ongoing_token = batch_size;
+    ssize_t ongoing_token = batch_size;
+
+    auto this_nid = dsm->get_node_id();
+    size_t next_nid = (this_nid + 1) % dsm->getClusterSize();
+
+    auto *qp = dsm->get_dir_qp(next_nid, tid, dir_id);
+    auto *mr = dsm->get_dir_mr(dir_id);
+    auto *cq = dsm->get_dir_cq(dir_id);
+    auto srv_buffer = dsm->get_server_internal_dsm_buffer();
+    ibv_wc wcs[128];
+    size_t mw_index = 0;
+    size_t constantly_active_mw_index = 0;
+    while (remain_task > 0)
     {
-        constexpr static ssize_t kAllocMwNr = 64;
-        static_assert(kAllocMwNr >= 4 * kBatchSize,
-                      "Recommend turn up kAllocMwNr.");
-        for (size_t t = 0; t < kAllocMwNr; ++t)
+        while (ongoing_token > 0)
         {
-            size_t dir_id = i % NR_DIRECTORY;
-            // NOTE: this dir_id should match the one below
-            mw_pool_threads[i].push_back(dsm->alloc_mw(dir_id));
-        }
-    }
+            ibv_mw_bind mw_bind;
+            mw_bind.wr_id = 0;
+            mw_bind.bind_info.mr = mr;
+            // get a little bit random by + 64 * mw_index
+            mw_bind.bind_info.addr = (uint64_t)(srv_buffer.buffer) +
+                                     64 * fast_pseudo_rand_int(0, 2_GB / 64);
+            // std::ignore = fast_pseudo_rand_int(0, 2_GB / 64);
+            // mw_bind.bind_info.addr = (uint64_t)(srv_buffer.buffer);
+            mw_bind.bind_info.length = bind_size;
+            mw_bind.bind_info.mw_access_flags = 31;  // magic, all access perm
+            mw_bind.send_flags |= IBV_SEND_SIGNALED;
 
-    // launcher (n-1) threads
-    ChronoTimer timer;
-    for (size_t i = 0; i < thread_nr; ++i)
-    {
-        threads.emplace_back([dsm,
-                              test_times,
-                              bind_size,
-                              tid = i,
-                              &mw_pool = mw_pool_threads[i]]() {
-            size_t dir_id = tid % NR_DIRECTORY;
-
-            ssize_t remain_task = test_times;
-            // ssize_t ongoing_token = batch_size;
-            ssize_t ongoing_token = kBatchSize;
-
-            auto *qp = dsm->get_dir_qp(0, 0, dir_id);
-            auto *mr = dsm->get_dir_mr(dir_id);
-            auto *cq = dsm->get_dir_cq(dir_id);
-            auto srv_buffer = dsm->get_server_internal_dsm_buffer();
-            ibv_wc wcs[128];
-            size_t mw_index = 0;
-            while (remain_task > 0)
+            // explain:
+            // the first @constantly_active_pool.size() MW
+            // will always be in the pool, so that they are
+            // always active.
+            ibv_mw *mw = nullptr;
+            if (unlikely(constantly_active_mw_index <
+                         constant_active_pool.size()))
             {
-                while (ongoing_token > 0)
-                {
-                    ibv_mw_bind mw_bind;
-                    mw_bind.wr_id = 0;
-                    mw_bind.bind_info.mr = mr;
-                    // get a little bit random by + 64 * mw_index
-                    // mw_bind.bind_info.addr =
-                    //     (uint64_t)(srv_buffer.buffer) +
-                    //     64 * fast_pseudo_rand_int(
-                    //              0, NR_DIRECTORY * 32 * kBatchSize);
-                    mw_bind.bind_info.addr = (uint64_t)(srv_buffer.buffer);
-                    mw_bind.bind_info.length = bind_size;
-                    mw_bind.bind_info.mw_access_flags =
-                        31;  // magic, all access perm
-                    mw_bind.send_flags |= IBV_SEND_SIGNALED;
-
-                    auto *mw = mw_pool[(mw_index++) % mw_pool.size()];
-                    int ret = ibv_bind_mw(qp, mw, &mw_bind);
-                    PLOG_IF(FATAL, ret) << "Failed to call ibv_bind_mw.";
-                    ongoing_token--;
-                    remain_task--;
-                }
-                int polled = ibv_poll_cq(cq, 128, wcs);
-                PLOG_IF(FATAL, polled < 0) << "Failed to call ibv_poll_cq";
-                ongoing_token += polled;
+                mw = constant_active_pool[constantly_active_mw_index++];
             }
-        });
-    }
-    // run one thread
-    // bench_mw_do(dsm, 0, test_times, bind_size, mw_pool_threads[0],
-    // kBatchSize);
-
-    for (auto &thread : threads)
-    {
-        thread.join();
-    }
-
-    auto ns = timer.pin();
-
-    size_t total_test_times = test_times * thread_nr;
-    LOG(INFO) << "[bench] bind MW with threads " << thread_nr << ", "
-              << util::pre_ops(total_test_times, ns, true)
-              << ", per_thread: " << util::pre_ops(test_times, ns, true);
-
-    // std::vector<std::vector<ibv_mw *>> mw_pool_threads;
-    for (const auto &arr : mw_pool_threads)
-    {
-        for (auto *mw : arr)
-        {
-            dsm->free_mw(mw);
+            else
+            {
+                mw = mw_pool[(mw_index++) % mw_pool.size()];
+            }
+            int ret = ibv_bind_mw(qp, mw, &mw_bind);
+            PLOG_IF(FATAL, ret) << "Failed to call ibv_bind_mw.";
+            ongoing_token--;
+            remain_task--;
         }
+        int polled = ibv_poll_cq(cq, 128, wcs);
+        PLOG_IF(FATAL, polled < 0) << "Failed to call ibv_poll_cq";
+        ongoing_token += polled;
     }
 }
 
-void bench_mw()
+void enter_bench_mw(DSM::pointer dsm,
+                    boost::barrier &bar,
+                    size_t bind_size,
+                    size_t test_times)
+{
+    constexpr static ssize_t kBatchSize = 4;
+    constexpr static ssize_t kAllocMwNr = 4;
+    // constexpr static ssize_t kAllocMwNr = 1024;
+    constexpr static ssize_t kConstantlyActiveMwNr = 10240;
+
+    for (size_t thread_nr : {1, 4, 8, 16})
+    {
+        auto tid = dsm->get_thread_id();
+        bool need_to_run = (size_t) tid < thread_nr;
+        // LOG_IF(INFO, need_to_run) << "[debug] tid " << tid << ", need to
+        // run";
+
+        static_assert(kAllocMwNr >= kBatchSize,
+                      "Recommend turn up kAllocMwNr.");
+
+        std::vector<ibv_mw *> mw_pool;
+        std::vector<ibv_mw *> constantly_active_mw;
+        if (need_to_run)
+        {
+            auto dir_id = tid;
+            for (size_t i = 0; i < kConstantlyActiveMwNr; ++i)
+            {
+                constantly_active_mw.push_back(dsm->alloc_mw(dir_id));
+            }
+            for (size_t i = 0; i < kAllocMwNr; ++i)
+            {
+                mw_pool.push_back(dsm->alloc_mw(dir_id));
+            }
+        }
+
+        bar.wait();
+        ChronoTimer timer;
+        if (need_to_run)
+        {
+            do_bench_mw(dsm,
+                        test_times,
+                        bind_size,
+                        kBatchSize,
+                        constantly_active_mw,
+                        mw_pool);
+        }
+        bar.wait();
+        auto ns = timer.pin();
+
+        size_t total_test_times = test_times * thread_nr;
+        LOG_IF(INFO, tid == 0)
+            << "[bench] bind MW with threads " << thread_nr << ", "
+            << util::pre_ops(total_test_times, ns, true)
+            << ", per_thread: " << util::pre_ops(test_times, ns, true);
+
+        for (ibv_mw *mw : mw_pool)
+        {
+            dsm->free_mw(mw);
+        }
+        for (ibv_mw *mw : constantly_active_mw)
+        {
+            dsm->free_mw(mw);
+        }
+        mw_pool.clear();
+
+        bar.wait();
+    }
+}
+
+void bench_mw_launcher(size_t bind_size, size_t test_times)
 {
     DSMConfig config;
     config.machineNR = ::config::kMachineNr;
     auto dsm = DSM::getInstance(config);
-    // bool succ = dsm->registerThread();
-    // LOG(INFO) << "I am master thread. register: " << succ;
-    // for (size_t thread_nr : {1, 4, 8, 16, 32})
-    for (size_t thread_nr : {1, 4, 8, 16})
-    // for (size_t thread_nr : {1})
+    dsm->registerThread();
+    bool succ = dsm->registerThread();
+    LOG(INFO) << "I am master thread. register: " << succ;
+
+    std::vector<std::thread> threads;
+    boost::barrier bar(kMaxAppThread);
+
+    for (size_t i = 1; i < kMaxAppThread; ++i)
     {
-        bench_mw_launcher(dsm, 64, 2_M, thread_nr);
+        threads.emplace_back([dsm, &bar, bind_size, test_times]() {
+            dsm->registerThread();
+            enter_bench_mw(dsm, bar, bind_size, test_times);
+        });
+    }
+    enter_bench_mw(dsm, bar, bind_size, test_times);
+
+    for (auto &thread : threads)
+    {
+        thread.join();
     }
 }
 
@@ -582,7 +629,7 @@ int main(int argc, char *argv[])
     // bench_rereg_mr(4_MB, 2000, true);
     // bench_dereg_mr(16_GB, 100, true);
 
-    bench_mw();
+    bench_mw_launcher(64, 2_M);
 
     LOG(INFO) << "finished. ctrl+C to quit.";
 }
