60367afd7b71c7e7e24611e05c401e141e427a48
yanbin <yanb20@mails.tsinghua.edu.cn>
2022-09-26 20:03:27 +0800

    critical: anomaly, give a snapshot
    bench_prot_lat.cpp:
    Line 539 bool succ = dsm->registerThread()
    If comment out, binding MW is super fast.
    If not comment out, is super slow and no scalable. why?

--- bench/bench_prot_lat.cpp
+++ bench/bench_prot_lat.cpp
@@ -383,6 +383,169 @@ void bench_qp_query_lat()
               << util::pre_ops(test_times, ns, true);
 }
 
+void bench_mw_do(DSM::pointer dsm,
+                 size_t tid,
+                 size_t test_times,
+                 size_t bind_size,
+                 std::vector<ibv_mw *> &mw_pool,
+                 size_t batch_size)
+{
+    // we are mocking server, so it should be tid.
+    size_t dir_id = tid % NR_DIRECTORY;
+
+    ssize_t remain_task = test_times;
+    ssize_t ongoing_token = batch_size;
+
+    auto *qp = dsm->get_dir_qp(0, 0, dir_id);
+    auto *mr = dsm->get_dir_mr(dir_id);
+    auto *cq = dsm->get_dir_cq(dir_id);
+    auto srv_buffer = dsm->get_server_internal_dsm_buffer();
+    ibv_wc wcs[128];
+    size_t mw_index = 0;
+    while (remain_task > 0)
+    {
+        while (ongoing_token > 0)
+        {
+            ibv_mw_bind mw_bind;
+            mw_bind.wr_id = 0;
+            mw_bind.bind_info.mr = mr;
+            // get a little bit random by + 64 * mw_index
+            // mw_bind.bind_info.addr =
+            //     (uint64_t)(srv_buffer.buffer) +
+            //     64 * fast_pseudo_rand_int(
+            //              0, NR_DIRECTORY * 32 * kBatchSize);
+            mw_bind.bind_info.addr = (uint64_t)(srv_buffer.buffer);
+            mw_bind.bind_info.length = bind_size;
+            mw_bind.bind_info.mw_access_flags = 31;  // magic, all access perm
+            mw_bind.send_flags |= IBV_SEND_SIGNALED;
+
+            auto *mw = mw_pool[(mw_index++) % mw_pool.size()];
+            int ret = ibv_bind_mw(qp, mw, &mw_bind);
+            PLOG_IF(FATAL, ret) << "Failed to call ibv_bind_mw.";
+            ongoing_token--;
+            remain_task--;
+        }
+        int polled = ibv_poll_cq(cq, 128, wcs);
+        PLOG_IF(FATAL, polled < 0) << "Failed to call ibv_poll_cq";
+        ongoing_token += polled;
+    }
+}
+
+void bench_mw_launcher(DSM::pointer dsm,
+                       size_t bind_size,
+                       size_t test_times,
+                       size_t thread_nr)
+{
+    constexpr static ssize_t kBatchSize = 4;
+
+    std::vector<std::thread> threads;
+    std::vector<std::vector<ibv_mw *>> mw_pool_threads;
+    mw_pool_threads.resize(thread_nr);
+    for (size_t i = 0; i < thread_nr; ++i)
+    {
+        constexpr static ssize_t kAllocMwNr = 64;
+        static_assert(kAllocMwNr >= 4 * kBatchSize,
+                      "Recommend turn up kAllocMwNr.");
+        for (size_t t = 0; t < kAllocMwNr; ++t)
+        {
+            size_t dir_id = i % NR_DIRECTORY;
+            // NOTE: this dir_id should match the one below
+            mw_pool_threads[i].push_back(dsm->alloc_mw(dir_id));
+        }
+    }
+
+    // launcher (n-1) threads
+    ChronoTimer timer;
+    for (size_t i = 0; i < thread_nr; ++i)
+    {
+        threads.emplace_back([dsm,
+                              test_times,
+                              bind_size,
+                              tid = i,
+                              &mw_pool = mw_pool_threads[i]]() {
+            size_t dir_id = tid % NR_DIRECTORY;
+
+            ssize_t remain_task = test_times;
+            // ssize_t ongoing_token = batch_size;
+            ssize_t ongoing_token = kBatchSize;
+
+            auto *qp = dsm->get_dir_qp(0, 0, dir_id);
+            auto *mr = dsm->get_dir_mr(dir_id);
+            auto *cq = dsm->get_dir_cq(dir_id);
+            auto srv_buffer = dsm->get_server_internal_dsm_buffer();
+            ibv_wc wcs[128];
+            size_t mw_index = 0;
+            while (remain_task > 0)
+            {
+                while (ongoing_token > 0)
+                {
+                    ibv_mw_bind mw_bind;
+                    mw_bind.wr_id = 0;
+                    mw_bind.bind_info.mr = mr;
+                    // get a little bit random by + 64 * mw_index
+                    // mw_bind.bind_info.addr =
+                    //     (uint64_t)(srv_buffer.buffer) +
+                    //     64 * fast_pseudo_rand_int(
+                    //              0, NR_DIRECTORY * 32 * kBatchSize);
+                    mw_bind.bind_info.addr = (uint64_t)(srv_buffer.buffer);
+                    mw_bind.bind_info.length = bind_size;
+                    mw_bind.bind_info.mw_access_flags =
+                        31;  // magic, all access perm
+                    mw_bind.send_flags |= IBV_SEND_SIGNALED;
+
+                    auto *mw = mw_pool[(mw_index++) % mw_pool.size()];
+                    int ret = ibv_bind_mw(qp, mw, &mw_bind);
+                    PLOG_IF(FATAL, ret) << "Failed to call ibv_bind_mw.";
+                    ongoing_token--;
+                    remain_task--;
+                }
+                int polled = ibv_poll_cq(cq, 128, wcs);
+                PLOG_IF(FATAL, polled < 0) << "Failed to call ibv_poll_cq";
+                ongoing_token += polled;
+            }
+        });
+    }
+    // run one thread
+    // bench_mw_do(dsm, 0, test_times, bind_size, mw_pool_threads[0],
+    // kBatchSize);
+
+    for (auto &thread : threads)
+    {
+        thread.join();
+    }
+
+    auto ns = timer.pin();
+
+    size_t total_test_times = test_times * thread_nr;
+    LOG(INFO) << "[bench] bind MW with threads " << thread_nr << ", "
+              << util::pre_ops(total_test_times, ns, true)
+              << ", per_thread: " << util::pre_ops(test_times, ns, true);
+
+    // std::vector<std::vector<ibv_mw *>> mw_pool_threads;
+    for (const auto &arr : mw_pool_threads)
+    {
+        for (auto *mw : arr)
+        {
+            dsm->free_mw(mw);
+        }
+    }
+}
+
+void bench_mw()
+{
+    DSMConfig config;
+    config.machineNR = ::config::kMachineNr;
+    auto dsm = DSM::getInstance(config);
+    // bool succ = dsm->registerThread();
+    // LOG(INFO) << "I am master thread. register: " << succ;
+    // for (size_t thread_nr : {1, 4, 8, 16, 32})
+    for (size_t thread_nr : {1, 4, 8, 16})
+    // for (size_t thread_nr : {1})
+    {
+        bench_mw_launcher(dsm, 64, 2_M, thread_nr);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     google::InitGoogleLogging(argv[0]);
@@ -404,12 +567,22 @@ int main(int argc, char *argv[])
     // clang-format off
     /**
      * rereg: 137ms per op [hugepage, 16GB, 100 op] (16.7 us per hugepage)
+     *  - rereg between "remote R+W", "local only"
+     *  - rereg between "remote R+W", "remote RO"
+     * The above two cases have the same latency. We conclude that:
+     * MR reregister consistently requires page unpinning & pinning.
      * rereg: 12.4s per op [page, 16GB, 10 op] (3 us per page, i.e., 1536 us per hugepages) 
      * dereg: 136ms per op [hugepage, 16GB, 100op]
      */
     // clang-format on
-    bench_rereg_mr(16_GB, 500, true);
+    // bench_rereg_mr(16_GB, 100, true);
+    // bench_rereg_mr(1, 200000, true);
+    // bench_rereg_mr(4_KB, 2000, true);
+    // bench_rereg_mr(2_MB, 2000, true);
+    // bench_rereg_mr(4_MB, 2000, true);
     // bench_dereg_mr(16_GB, 100, true);
+
+    bench_mw();
 
     LOG(INFO) << "finished. ctrl+C to quit.";
 }
